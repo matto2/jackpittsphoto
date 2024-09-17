4a07d396efbeeb412023007e9eea25a62eac0f1f
Matt O'Leary <matto2@users.noreply.github.com>
2024-09-17 13:20:12 -0700

    second

---
+++ NOTES.md
@@ -0,0 +1,8 @@
+A photograph is a secret about a secret.
+Diane Arbus
+
+If we could see the miracle of a single flower - clearly - our whole lives would change.
+The Buddah
+
+The principle of true art is not to portray, but to evoke.
+Jerzy Kosinski
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				redrose: ['"Red Rose"', 'sans-serif'], // Add Red Rose to font family
+			  },
 	},
 	plugins: [],
+	},
 }
