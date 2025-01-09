9cc768d10d53846a48e162736565954e29205ae6
Matt O'Leary <matto2@users.noreply.github.com>
2025-01-09 11:51:21 -0800

    upgraded astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind()],
 });
